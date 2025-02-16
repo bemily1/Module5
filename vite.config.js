e47591887e6fe8fd036390c520ac0d912914ad84
bemily1 <156600946+bemily1@users.noreply.github.com>
2025-02-16 17:05:47 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/Module5/', // Set the base path for GitHub Pages
   plugins: [react()],
 })
